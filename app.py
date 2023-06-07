c3fec4a285cb602b289ca52f295757e7d73a8787
Ritesh Zode <riteshzode1122@gmail.com>
2023-06-07 19:37:04 +0530

    Dockerfile and requirements.txt added

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("meme_index.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
